8ad2f5a5651e946046d6041330031eb1479b98b2
wteam-xq <857609086@qq.com>
2014-11-01 17:12:46 +0800

    进度条优化

--- js/main.js
+++ js/main.js
@@ -1,26 +1,42 @@
 //页面加载后执行， dom 加载完成
 $(document).ready(function(){
-  // 进度表 1秒加载完毕
-  var pro_val = 0;   //进度值
-  var timer = null;  //setTimeout对象
-  var $progre = $("div.progress-bar");
-  $progre.css('width', '0%');
+  
+  // // 进度表 1秒加载完毕
+  // var pro_val = 0;   //进度值
+  // var timer = null;  //setTimeout对象
+  // var dom_progress = document.getElementById('progress_bar');
+  // dom_progress.style.width = '0%';
+  // var dom_proTxt = dom_progress.getElementsByTagName('span')[0];
 
-  setTimeout(updateProgress, 100);
+  // setTimeout(updateProgress, 50);
+  // function updateProgress(){
 
-  function updateProgress(){
-    if(pro_val == 100){
-      clearTimeout(timer);
-      $("#start").hide();
+  //   dom_progress.setAttribute('aria-valuenow', pro_val);
+  //   dom_progress.style.width = pro_val + '%';
+  //   dom_progress.width = pro_val + '%';
+  //   dom_proTxt.innerHTML = pro_val + '%';
+  //   if(pro_val == 100){
+  //     clearTimeout(timer);
+  //     $("#start").hide();
+  //     $('#mainmenu').show();
+  //     return false;
+  //   }
+  //   pro_val+=1;
+  //   timer = setTimeout(updateProgress, 50);
+  // }
+  
+  // 进图条插件
+  var options = {
+     speed: 20,
+     limit: 100,
+     onComplete: function(pro_val){
+      // console.log(pro_val);
+      $("#progress").hide();
       $('#mainmenu').show();
-      return;
-    }
-    $progre.attr('aria-valuenow', pro_val);
-    $progre.css('width', pro_val + '%');
-    $progre.find('.txt').text(pro_val + '%');
-    pro_val+=1;
-    timer = setTimeout(updateProgress, 100);
-  }
+     }
+  };
+  var myplugin = $('#progress').cprogress(options);
+  myplugin.start();
   
   $("#heros").click(function(){
   window.location.href="herosMainPage/heros.html";
