6a0c4f37328a36fe87f6564442f66be44a5e2235
wteam-xq <857609086@qq.com>
2014-11-07 22:33:03 +0800

    删除 多余代码

--- js/main.js
+++ js/main.js
@@ -1,34 +1,6 @@
 //页面加载后执行， dom 加载完成
 $(document).ready(function(){
   
-  // bootstrap进度条初始化
-  function bProgressInit(){
-    // 进度表 1秒加载完毕
-    var pro_val = 0;   //进度值
-    var timer = null;  //setTimeout对象
-    var dom_progress = document.getElementById('progress_bar');
-    dom_progress.style.width = '0%';
-    var dom_proTxt = dom_progress.getElementsByTagName('span')[0];
-
-    setTimeout(updateProgress, 50);
-
-    function updateProgress(){
-      dom_progress.setAttribute('aria-valuenow', pro_val);
-      dom_progress.style.width = pro_val + '%';
-      dom_progress.width = pro_val + '%';
-      dom_proTxt.innerHTML = pro_val + '%';
-      if(pro_val == 100){
-        clearTimeout(timer);
-        $("#start").hide();
-        $('#mainmenu').show();
-        return false;
-      }
-      pro_val+=1;
-      timer = setTimeout(updateProgress, 50);
-    }
-  }
-  // bProgress();
-  
   // 插件进度条初始化
   function pProgressInit(){
     // 进图条插件
@@ -121,18 +93,5 @@ $(document).ready(function(){
   createGroupItem(card_datas, $('#card') );
   createToggleBtn(str_datas, $('#strategy') );
   
-
-  // $("#heros").click(function(){
-  // window.location.href="herosMainPage/heros.html";
-  // }); //end click function()
-  // $("#card").click(function(){
-  // window.location.href="herosMainPage/card.html";
-  // });
-  // $("#rule").click(function(){
-  // window.location.href="herosMainPage/rule.html";
-  // });
-  //  $("#strategy").click(function(){
-  // window.location.href="herosMainPage/strategy.html";
-  // });
   
 });
